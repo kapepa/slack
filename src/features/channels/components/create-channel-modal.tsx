ca4c1103c976639330d5681c83b994001f691d3a
kapepa <kapepayx@gmail.com>
2024-09-02 17:49:15 +0300

    creare channel

---
+++ src/features/channels/components/create-channel-modal.tsx
@@ -0,0 +1,84 @@
+"use client"
+
+import { FC, FormEvent, FormHTMLAttributes, useState } from "react";
+import { useCreateChannelModal } from "../store/use-create-channel-modal";
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useCreateChannel } from "../api/use-create-channels";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+
+const CreateChannelModal: FC = () => {
+  const [open, setOpen] = useCreateChannelModal();
+  const [name, setName] = useState<string>("");
+
+  const workspaceId = useWorkspaceId();
+  const { mutate, isPending } = useCreateChannel();
+
+  const handleClose = () => {
+    setName("");
+    setOpen(false);
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.replace(/\s+/g, "-").toLowerCase();
+    setName(value);
+  }
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    mutate(
+      { name, workspaceId },
+      {
+        onSuccess(id) {
+          handleClose()
+        },
+      }
+    )
+  }
+
+  return (
+    <Dialog
+      open={open}
+      onOpenChange={handleClose}
+    >
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>
+            Add a Channel
+          </DialogTitle>
+        </DialogHeader>
+        <form
+          className="space-y-4"
+          onSubmit={handleSubmit}
+        >
+          <Input
+            value={name}
+            onChange={handleChange}
+            required
+            autoFocus
+            minLength={3}
+            maxLength={80}
+            disabled={isPending}
+            placeholder="e.g. plan-budget"
+            type="text"
+            name="plan"
+          />
+          <div
+            className="flex justify-end"
+          >
+            <Button
+              disabled={isPending}
+              type="submit"
+            >
+              Create
+            </Button>
+          </div>
+        </form>
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+export { CreateChannelModal }
